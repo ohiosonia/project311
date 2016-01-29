fcb45b9c9276d64b3cce7f46a0f59afbc19d6803
smehta930 <smehta930@gmail.com>
2016-01-28 22:15:11 -0500

    Updating kafka

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -8,8 +8,8 @@ from kafka.producer import KeyedProducer
 class Producer(object):
 
   def __init__(self, addr):
-    self.client = "" #KafkaClient(addr)
-    self.producer = "" # KeyedProducer(self.client)
+    self.client = KafkaClient(addr)
+    self.producer = KeyedProducer(self.client)
     self.zipcode = []
     self.complaint = []
 
@@ -34,7 +34,7 @@ class Producer(object):
       complaint_field = random.choice(self.complaint)
       str_fmt = "{};{};{};{}"
       message_info = str_fmt.format(source_symbol, time_field, zipcode_field, complaint_field)
-      # print message_info
+      print message_info
       self.producer.send_messages('complaint', source_symbol, message_info)
       msg_cnt += 1
 
