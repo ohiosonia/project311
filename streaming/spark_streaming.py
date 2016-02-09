118242290f14a1ec3e396d103a7ca1977fb2384b
Ubuntu <smehta930@gmail.com>
2016-02-09 18:35:06 +0000

    Updating kafka streams

--- streaming/spark_streaming.py
+++ streaming/spark_streaming.py
@@ -5,10 +5,13 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark_cassandra import streaming
 
+def convert(source, info):
+    info_all = info.split(';')
+    return info_all
+   
 if __name__ == "__main__":
 
    # arguments
-   
    kafka_ip = "52.71.164.204"
    # configure spark instance
    conf = (SparkConf().setMaster("spark://ip-172-31-0-134:7077")\
@@ -23,17 +26,15 @@ if __name__ == "__main__":
    ssc = StreamingContext(sc, 5)
 
    data = KafkaUtils.createStream(ssc, "%s:2181"%kafka_ip, "complaints",{"complaints":1})
-   print type(data)
-   parsed = data.map(lambda (something, json_line): json.loads(json_line))
- #  extracted = parsed.map(lambda message: (parse_dictionary_retrieve_date(message))).filter(lambda x: len(x) > 0)  
-  # print 'extracted ', extracted.pprint()
+   #data.pprint()
+   parsed = data.map(lambda (in_tuple): convert(in_tuple[0], in_tuple[1]))
+   parsed.pprint()
    my_row = parsed.map(lambda x: {
    "source": x[0],
    "time": x[1],
    "zipcode": x[2],
    "complaint": x[3]
     })
-
    #mapped = extracted.map(lambda line: line[0][0], line[0][1], line[1])) # return a list
    my_row.saveToCassandra("playground", "live_complaints2" )# save RDD to cassandra
    ssc.start() # start the process
